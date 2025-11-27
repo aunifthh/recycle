b95e917ead962b2d47735b9e636997e60abff6f8
aunifthh <auniauzi@gmail.com>
2025-11-27 16:32:17 +0800

    base user interface

---
+++ sidebar/usersidebar.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css">
+
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="dashboard.php" class="brand-link">
+        <img
+            src="../images/truck.png"
+            alt="Recycle Logo"
+            class="brand-image"
+        />
+        <span class="brand-text font-weight-light">Recycle</span>
+    </a>
+
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="info">
+                <a class="d-block"><i class="bi bi-person" style="margin-right:3px;"></i>User</a>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+
+                <li class="nav-item">
+                    <a href="../user/dashboard.php" class="nav-link <?= ($currentPage == 'dashboard' ? 'active' : '') ?>">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="../user/pickups.php" class="nav-link <?= ($currentPage == 'pickups' ? 'active' : '') ?>">
+                        <i class="nav-icon fas fa-truck"></i>
+                        <p>Pickup Requests</p>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+</aside>
---
+++ user/dashboard.php
@@ -0,0 +1,49 @@
+<?php
+// session_start();
+$currentPage = 'dashboard';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="icon" type="image/png" href="../images/truck.png">
+
+    <!-- AdminLTE -->
+    <link rel="stylesheet" href="../app/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../app/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="../app/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="../app/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    <?php include("../navbar/usernavbar.php"); ?>
+    <?php include("../sidebar/usersidebar.php"); ?>
+
+    <!-- Content Wrapper -->
+    <div class="content-wrapper">
+        <section class="content-header">
+            <div class="container-fluid">
+                <h3 class="mb-2">Dashboard</h3>
+            </div>
+        </section>
+    </div>
+
+    <?php include("../footer/userfooter.php"); ?>
+</div>
+
+
+<!-- Scripts -->
+<script src="../app/plugins/jquery/jquery.min.js"></script>
+<script src="../app/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../app/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../app/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../app/dist/js/adminlte.min.js"></script>
+
+
+
+</body>
+</html>
